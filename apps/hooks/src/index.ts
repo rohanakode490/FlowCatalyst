c5279778d289e8d04c28a614016d09a4a0a769a2
Rohan Akode <rohanakode52@gmail.com>
2024-12-01 22:12:58 +0530

    test

--- apps/hooks/src/index.ts
+++ apps/hooks/src/index.ts
@@ -1,34 +1,34 @@
-import express from "express"
-import { prisma, Prisma } from "@repo/database"
+import express from "express";
+import { prisma, Prisma } from "@repo/database";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 // Password Logic
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
-    const userId = req.params.userId;
-    const zapId = req.params.zapId;
-    const body = req.body;
-    
-    // Store in db a new trigger
-    const result = await prisma.$transaction(async (tx:Prisma.TransactionClient) => {
-        const run = await tx.zapRun.create({
-            data: {
-                zapId: zapId,
-                metadata: body
-            }
-        })
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
 
-        await tx.zapRunOutbox.create({
-            data: {
-                zapRunId: run.id,
-            }
-        })
-    })
-    res.json({
-        message: "Webhook received successfully"
-    })
-    // TODO: push it to a queue(redis/kafka)
-})
+  // Store in db a new trigger
+  await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
+    });
 
-app.listen(3000)
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+  res.json({
+    message: "Webhook received successfully",
+  });
+  // TODO: push it to a queue(redis/kafka)
+});
+
+app.listen(3000);
