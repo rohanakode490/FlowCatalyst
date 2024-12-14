ca06958269b20064924cd2a97a952817796f49a3
Rohan Akode <rohanakode52@gmail.com>
2024-12-14 15:03:28 +0000

    Fix: Schema table for Zap, User and added 'sortingOrder' for Actions

---
+++ packages/database/prisma/migrations/20241213160819_add_user_id_to_zap/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Action" ADD COLUMN     "sortingOrder" INTEGER NOT NULL DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "Zap" ADD COLUMN     "userId" INTEGER NOT NULL DEFAULT 1;
+
+-- AddForeignKey
+ALTER TABLE "Zap" ADD CONSTRAINT "Zap_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ packages/database/prisma/migrations/20241213160933_remove_default_from_user_id/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Zap" ALTER COLUMN "userId" DROP DEFAULT;
